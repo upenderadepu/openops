65770f209d77916ee2655c3cb134d2187246aef6
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-03-11 15:20:53 +0000

    Adjust monday auth message (#89)

--- packages/blocks/monday/src/index.ts
+++ packages/blocks/monday/src/index.ts
@@ -15,8 +15,9 @@ const markdown = `
 1.Log into your monday.com account.\n
 2.Click on your avatar/profile picture in the top right corner.\n
 3.Select **Administration** (this requires you to have admin permissions).\n
-4.Go to the **API** section.\n
-5.Copy your personal token`;
+4.Go to **Connections** section.\n
+5.Select **API** tab.\n
+6.Copy your personal token`;
 
 export const mondayAuth = BlockAuth.SecretText({
   displayName: 'API v2 Token',
