e1d75f8dfa67c99d120ed0512d9c79fba0cf6165
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-03-12 21:00:57 +0100

    Hide Create File action (#151)

--- packages/blocks/file-helper/src/index.ts
+++ packages/blocks/file-helper/src/index.ts
@@ -1,6 +1,5 @@
 import { BlockAuth, createBlock } from '@openops/blocks-framework';
 import { BlockCategory } from '@openops/shared';
-import { createFile } from './lib/actions/create-file';
 import { readFileAction } from './lib/actions/read-file';
 
 export const filesHelper = createBlock({
@@ -11,6 +10,6 @@ export const filesHelper = createBlock({
   logoUrl: 'https://static.openops.com/blocks/file-block.svg',
   categories: [BlockCategory.CORE],
   authors: ['kishanprmr', 'MoShizzle', 'abuaboud'],
-  actions: [readFileAction, createFile],
+  actions: [readFileAction],
   triggers: [],
 });
