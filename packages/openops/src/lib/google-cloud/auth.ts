04a7855ecea2871b0e5c4a5907c9a5bf1a2e6d3d
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-03-27 16:49:53 +0100

    Add documentation how to add GCP auth (#321)

--- packages/openops/src/lib/google-cloud/auth.ts
+++ packages/openops/src/lib/google-cloud/auth.ts
@@ -9,7 +9,19 @@ import { useTempFile } from '../use-temp-file';
 const enableHostSession =
   system.getBoolean(SharedSystemProp.ENABLE_HOST_SESSION) ?? false;
 
+const markdown = `
+1. Log into Google Cloud console.\n
+2. Go to **IAM and admin**.\n
+3. On the left sidebar, click on **Service accounts**.\n
+4. Create a new service account and grant it permissions for the relevant projects, or select an existing service account.\n
+5. After selecting the service account, click on the tab **Keys**, and create a new JSON key.\n
+6. Open the downloaded file, and copy the content.\n
+7. Paste the content in the field below.\n
+
+You can also visit [OpenOps documentation](https://docs.openops.com/introduction/overview) for more information.`;
+
 export const googleCloudAuth = BlockAuth.CustomAuth({
+  description: markdown,
   props: {
     keyFileContent: BlockAuth.SecretText({
       displayName: 'Key file content',
