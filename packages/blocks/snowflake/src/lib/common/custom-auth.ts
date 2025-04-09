47fddcde277745aede8fe22fa5c89b0910bc127e
Cezar <cezar@openops.com>
2025-04-09 17:28:00 +0300

    Improve connection docs wording for Max Login Retries in relation to incorrect account identifier (#427)

--- packages/blocks/snowflake/src/lib/common/custom-auth.ts
+++ packages/blocks/snowflake/src/lib/common/custom-auth.ts
@@ -12,7 +12,7 @@ const markdown = `
 
 For the **Password**, you will need to provide the same password you use to log in to your Snowflake account.
 
-**Important:** Increasing the default **maxLoginRetries** setting can significantly extend response times if the **Account Identifier** is incorrect. The system will repeatedly attempt to connect, potentially delaying feedback before ultimately failing with a generic error message: "Request to Snowflake failed.".
+If you're experiencing delays, double-check that your **Account Identifier** is correct. An incorrect identifier combined with an increased **Max Login Retries** value will cause the system to repeatedly try connecting, resulting in longer wait times before it eventually fails with the error: "Request to Snowflake failed."
 `;
 
 export const customAuth = BlockAuth.CustomAuth({
