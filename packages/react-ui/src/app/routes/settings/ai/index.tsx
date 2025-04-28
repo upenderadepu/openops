6047dba4bd8946a720320969d903fafda7c4a6fa
Roman Snapko <roman@openops.com>
2025-04-28 12:08:17 +0300

    Update AI settings page title (#509)

--- packages/react-ui/src/app/routes/settings/ai/index.tsx
+++ packages/react-ui/src/app/routes/settings/ai/index.tsx
@@ -65,7 +65,7 @@ const AiSettingsPage = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-4">
       <div className="mx-auto w-full flex-col">
-        <h1 className="text-2xl font-bold mb-[35px]">{t('Ai providers')}</h1>
+        <h1 className="text-2xl font-bold mb-[35px]">{t('AI providers')}</h1>
         <div className="flex justify-between p-6 border rounded-[11px]">
           <AiSettingsForm
             aiProviders={aiProviders}
