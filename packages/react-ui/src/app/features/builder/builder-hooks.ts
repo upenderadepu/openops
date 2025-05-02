2146985c8a91cae122ba871c8b6fc0afa37fe9f8
Alexandru-Dan Pop <alexandrudan@openops.com>
2025-05-02 17:11:47 +0300

    Select dragged step (#543)

--- packages/react-ui/src/app/features/builder/builder-hooks.ts
+++ packages/react-ui/src/app/features/builder/builder-hooks.ts
@@ -192,9 +192,10 @@ export const createBuilderStore = (initialState: BuilderInitialState) =>
           ),
         setActiveDraggingStep: (stepName: string | null) =>
           set(
-            {
+            (state) => ({
               activeDraggingStep: stepName,
-            },
+              selectedStep: stepName ?? state.selectedStep,
+            }),
             false,
             'setActiveDraggingStep',
           ),
